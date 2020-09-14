f8e856183a2d519e0666a97cae91436fbfd550f9
Eric Alas <eric.alas@sap.com>
2020-09-14 16:01:31 -0400

    Initial Commit

---
+++ PlantCare/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlantCare
+//
+//  Created by Alas, Eric on 2020-09-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlantCare/PlantCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantCareApp.swift
+//  PlantCare
+//
+//  Created by Alas, Eric on 2020-09-14.
+//
+
+import SwiftUI
+
+@main
+struct PlantCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
